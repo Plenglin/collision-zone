e0a30a43104f524ce248832ec495ebdbcefc76b4
Maxim Yu <maxim.yu314@gmail.com>
2019-06-10 19:57:35 +0800

    Add some convenience logging macros

--- src/util/log.hpp
+++ src/util/log.hpp
@@ -18,9 +18,18 @@
 #define LOGGING_TAG_ATTR_NAME "Tag"
 #define LOGGING_DEFAULT_TAG "NO-TAG"
 
+using namespace boost::log::trivial;
+
+#define LOG_TRACE(logger)  BOOST_LOG_SEV(logger, trace)
+#define LOG_DEBUG(logger)  BOOST_LOG_SEV(logger, debug)
+#define LOG_INFO(logger)   BOOST_LOG_SEV(logger, info)
+#define LOG_WARN(logger)   BOOST_LOG_SEV(logger, warning)
+#define LOG_ERROR(logger)  BOOST_LOG_SEV(logger, error)
+#define LOG_FATAL(logger)  BOOST_LOG_SEV(logger, fatal)
+
 namespace util {
 
-    typedef boost::log::sources::severity_logger<boost::log::trivial::severity_level> Logger;
+    typedef boost::log::sources::severity_logger<severity_level> Logger;
 
     void initialize_logging();
     Logger getLogger(std::string tag);
