552b57658ab5a00201401d906c2f3419ee57014f
Maxim Yu <maxim.yu314@gmail.com>
2019-06-10 19:51:55 +0800

    Seperate util impl and dec

--- src/snowplowderby/Client.hpp
+++ src/snowplowderby/Client.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
+#include "Arena.hpp"
 #include "Player.hpp"
+#include "util/log.hpp"
 
 
 namespace snowplowderby {
 
     class Client {
     private:
+        static util::Logger logger;
+        Arena* arena;
         Player* player;
     public:
         Client(/* args */);
         ~Client();
 
-        void setPlayer(Player* player);
+        void attach(Arena* arena, Player* player);
+        virtual void sendToClient() = 0;
     };
 
 }
