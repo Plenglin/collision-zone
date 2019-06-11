61364be48893546f39ea90138a55be95cfab5f20
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-06-11 22:11:13 +0800

    Resize canvas to fill

--- public/scripts/game/core.js
+++ public/scripts/game/core.js
@@ -1,1 +1,28 @@
+var config = {
+    type: Phaser.AUTO,
+    scale: {
+        parent: 'game-div',
+        mode: Phaser.Scale.RESIZE,
+        resizeInterval: 500
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
+console.info("Creating game");
+var game = new Phaser.Game(config);
+
+function preload() {
+    console.info("Preload");
+}
+
+function create() {
+    console.info("Create");
+}
+
+function update() {
+    console.debug("Update loop");
+}
