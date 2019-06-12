063c044502bb5e6f2ee0d1f916e47a06611b5d0d
Maxim Yu <maxim.yu314@gmail.com>
2019-06-12 17:27:45 +0800

    Make initial into bytes

--- src/snowplowderby/Player.hpp
+++ src/snowplowderby/Player.hpp
@@ -9,41 +9,33 @@
 #include "snowplowderby/io/s2c.hpp"
 
 #define USERDATA_TYPE_PLAYER 13498931
+#define PLAYER_NAME_LENGTH_LIMIT 20
 
 namespace snowplowderby {
 
     class Player {
     private:
         const short id;
+        const char car_class = 0;
         b2Body* body;
 
         bool alive = true;
 
         util::UserDataWrapper user_data;
-        std::string name;
+        char name[PLAYER_NAME_LENGTH_LIMIT] = {0};
     public:
         Player(short id, b2Body* body);
 
         short get_id();
+        char get_car_class();
         bool is_alive();
         bool is_boosting();
 
         util::UserDataWrapper* get_user_data();
 
         void write_update_bytes(std::ostream& os);
+        void write_initial_bytes(std::ostream& os);
 
-        template <typename Writer>
-        void serialize_initial(Writer& writer) const {
-            writer.StartObject();
-
-            writer.String("id");
-            writer.Int(id);
-
-            writer.String("name");
-            writer.String(name);
-
-            writer.EndObject();
-        }
     };
 
     typedef std::shared_ptr<Player> PlayerPtr;
