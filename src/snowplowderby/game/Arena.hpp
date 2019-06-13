3b11484e798cdd88d8e6b021af695abb87eb9e69
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-06-13 18:04:41 +0800

    Wall creation

--- src/snowplowderby/game/Arena.hpp
+++ src/snowplowderby/game/Arena.hpp
@@ -3,10 +3,12 @@
 #include <Box2D/Box2D.h>
 #include <Box2D/Common/b2Math.h>
 #include <list>
-#include <unordered_map>
 #include <ostream>
+#include <random>
+#include <unordered_map>
 
 #include "Player.hpp"
+#include "Wall.hpp"
 #include "util/log.hpp"
 
 #define UPDATE_PERIOD 50  // Milliseconds
@@ -15,14 +17,20 @@ namespace snowplowderby::game {
 
     class Arena {
         private:
+            static std::default_random_engine random;
+            static std::uniform_real_distribution<float> distribution;
+
             static util::Logger logger;
             b2World phys_world;
             std::unordered_map<short, PlayerPtr> players;
             std::list<PlayerPtr> new_players;
+            std::list<Wall> walls;
             short next_player_id = 0;
         public:
             Arena();
             ~Arena();
+
+            void create_random_wall();
 
             PlayerPtr create_player();
             void destroy_player(PlayerPtr player);
--- src/snowplowderby/game/Wall.hpp
+++ src/snowplowderby/game/Wall.hpp
@@ -1,11 +1,18 @@
+#include <Box2D/Box2D.h>
+#include <ostream>
+
+
 namespace snowplowderby::game {
 
-    class Wall {
-    private:
-        
-    public:
-        Wall();
-        ~Wall();
+    struct Wall {
+        float x;
+        float y;
+        float width;
+        float height;
+        float angle;
+
+        b2Body* create_body(b2World* world);
+        void write_initial_bytes(std::ostream& os);
     };
 
 }
