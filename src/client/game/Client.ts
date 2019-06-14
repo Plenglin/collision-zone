7e5d733e02fbb95a106f42311cce1d5464ef8b7b
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-06-14 11:11:38 +0800

    Send become player requests

--- src/client/game/Client.ts
+++ src/client/game/Client.ts
@@ -4,13 +4,16 @@ import { Wall } from "./Wall";
 import { GameScene } from "./GameScene";
 
 enum ClientState {
-    UNINITIALIZED, SPECTATING, PLAYING
+    UNINITIALIZED, SPECTATING, REQUESTING_TRANSITION_TO_PLAYING, PLAYING
 }
 
 export class Client {
     url: string
     socket: WebSocket
     state: ClientState
+
+    private resolveTransitionRequest: any
+    private rejectTransitionRequest: any
     
     constructor(url: string, private scene: GameScene) {
         this.url = url
@@ -18,22 +21,75 @@ export class Client {
         this.socket.onopen = () => {
             console.info("Socket opened at", url)
         }
-        this.socket.onmessage = (data) => this.onMessage(data)
-
-        this.state = ClientState.UNINITIALIZED
+        this.setUninitialized()
     }
 
-    async onMessage(data: MessageEvent) {
-        const arrayBuffer = await new Response(data.data).arrayBuffer()
-        if (this.state == ClientState.UNINITIALIZED) {
-            this.onInitializationMessage(arrayBuffer)
-            return
+    async requestTransitionToPlaying(request: BecomePlayerRequest): Promise<string> {
+        this.state = ClientState.REQUESTING_TRANSITION_TO_PLAYING;
+        // Reliable, and transition
+        this.socket.send("rt" + JSON.stringify(request));
+        return new Promise((resolve, reject) => {
+            this.resolveTransitionRequest = resolve;
+            this.rejectTransitionRequest = reject;
+        })
+    }
+
+    private setUninitialized() {
+        this.state = ClientState.UNINITIALIZED
+        this.socket.onmessage = (data) => {
+            this.onInitializationMessage(data.data)
         }
     }
 
-    onInitializationMessage(data: ArrayBuffer) {
+    private setSpectating() {
+        this.socket.onmessage = async (data) => {
+            const buf = await new Response(data.data).arrayBuffer()
+            const stream = new ByteArrayInputStream(buf)
+            if (stream.readByte() == 117) {  // unreliable
+                this.readPeriodicGameUpdate(stream)
+            } else {
+                switch (stream.readByte()) {
+                    case 41:  // Transition response
+                        this.readTransitionResponse(stream)
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private readPeriodicGameUpdate(stream: ByteArrayInputStream) {
+
+    }
+
+    private readTransitionResponse(stream: ByteArrayInputStream) {
+        const code = stream.readByte()
+        switch (code) {
+            case 0:
+                this.resolveTransitionRequest(null)
+                this.state = ClientState.PLAYING
+                return;
+            case 1:
+                this.rejectTransitionRequest('Malformed request')
+                break;
+            case 2:
+                this.rejectTransitionRequest('Username already taken')
+                break;
+            case 3:
+                this.rejectTransitionRequest('Username too long')
+                break;
+            case 4:
+                this.rejectTransitionRequest('Username empty')
+                break;
+        }
+        this.state = ClientState.SPECTATING
+    }
+
+    private async onInitializationMessage(blob: Blob) {
+        const data = await new Response(blob).arrayBuffer()
         console.debug("Received initialization message", data)
-        this.state = ClientState.SPECTATING;
+        this.setSpectating()
 
         const stream = new ByteArrayInputStream(data);
         stream.readByte();  // clear u/r
@@ -47,7 +103,12 @@ export class Client {
             const wall = Wall.readFromStream(this.scene, stream)
             this.scene.addWall(wall)
         }
-        // player init
+        // players initialization
     }
 
+}
+
+interface BecomePlayerRequest {
+    username: string;
+    player_class: integer;
 }
