0028bb3f0fcd2d92a355aa828bfc6073726c4ff7
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-06-16 21:25:49 +0800

    Fix camera centering. Rect centering is still bork

--- src/client/game/Wall.ts
+++ src/client/game/Wall.ts
@@ -5,6 +5,7 @@ import { GameObjects, Scene } from "phaser";
 export class Wall extends GameObjects.Rectangle {
     constructor(scene: Scene, x: number, y: number, w: number, h: number, a: number) {
         super(scene, x, y, w, h, 0xffffff)
+        this.setOrigin(0.5, 0.5)
         this.rotation = a
         this.isFilled = true
     }
