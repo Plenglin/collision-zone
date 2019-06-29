f8ef7980f9d16b986be5a80a1c0d486e36bb9464
Max <maximumpwnguin@gmail.com>
2019-06-29 16:45:06 -0700

    Move things around

--- assets/ts/game/protocol.ts
+++ assets/ts/game/protocol.ts
@@ -1,6 +1,6 @@
 import * as $ from 'jquery'
 import { ByteArrayInputStream } from "../util";
-import { GameScene } from "./GameScene";
+import { GameScene } from "./view/scene";
 import { GameObjects, Scene, Input } from "phaser";
 import { GameState, Player } from './gamestate';
 
@@ -37,10 +37,10 @@ export class Client {
     
     constructor(base_url: string, private scene: GameScene, player_data?: PlayerInitData) {
         if (player_data) {
-            this.url = base_url + `/play?username=${player_data.username}&class=${player_data.player_class}`
+            this.url = base_url + `?username=${player_data.username}&class=${player_data.player_class}`
             this.is_player = true
         } else {
-            this.url = base_url + '/spectate'
+            this.url = base_url
             this.is_player = false
         }
         this.socket = new WebSocket(this.url)
@@ -128,58 +128,4 @@ export class Client {
         this.socket.send(abuf)
     }
 
-}
-
-interface BecomePlayerRequest {
-    username: string;
-    player_class: integer;
-}
-
-export class PlayerInputHandler extends GameObjects.GameObject {
-    private pointer: Input.Pointer
-    private player: Player
-    constructor(scene: Scene, private client: Client) {
-        super(scene, 'player-input-handler')
-        this.pointer = this.scene.game.input.activePointer
-        this.player = client.player
-        // this.scene.input.setPollAlways()
-        // this.scene.input.on('pointermove', () => {
-        //     const p = scene.cameras.main.getWorldPoint(this.pointer.x, this.pointer.y)
-        //     const dx = p.x - this.player.x
-        //     const dy = p.y - this.player.y
-        //     console.debug(dx, dy)
-        //     client.setPlayerInput(dx, dy)
-        // })
-
-        const receiver = $('body')
-        receiver.mousemove((event) => {
-            const p = scene.cameras.main.getWorldPoint(event.pageX, event.pageY)
-            const dx = p.x - this.player.x
-            const dy = p.y - this.player.y
-            // console.debug(dx, dy)
-            client.set_player_input(dx, dy)
-        })
-        receiver.mousedown((event) => {
-            console.debug(event)
-            switch (event.button) {
-                case 0:  // Left
-                    client.send_boost()
-                    break;
-                case 2:  // Right
-                    client.send_brake(true)
-                    break;
-            }
-        })
-        receiver.mouseup((event) => {
-            console.debug(event)
-            switch (event.button) {
-                case 2:  // Right
-                    client.send_brake(false)
-                    break;
-            }
-        })
-        receiver.bind('contextmenu', (event) => {
-            return false
-        })
-    }
 }
--- assets/ts/game/view/player.ts
+++ assets/ts/game/view/player.ts
@@ -1,21 +1,12 @@
 import { GameObjects, Scene, Game } from "phaser"
-import { ByteArrayInputStream } from "../util";
+import { ByteArrayInputStream } from "../../util";
 
 const ALIVE_FLAG = 0x01
 const BRAKING_FLAG = 0x02
 const BOOSTING_FLAG = 0x04
 
 export class Player extends GameObjects.Container {
-    id: integer
-    vx: number
-    vy: number
-    omega: number
-    alive: boolean
-    braking: boolean
-    boosting: boolean
-    car_class: integer
-    player_name: string
-    kills: integer
+    id: integer = 0
 
     alive_sprite: GameObjects.Sprite
     invuln_sprite: GameObjects.Sprite
@@ -68,29 +59,6 @@ export class Player extends GameObjects.Container {
     
         this.dead_particle_emitter.stop()
         this.boost_particle_emitter.stop()
-
-        this.id = data.id
-        this.car_class = data.car_class
-        this.player_name = data.name
-        this.kills = data.kills
-        this.applyServerUpdate(data)
-    }
-
-    applyServerUpdate(data: UpdatePlayer) {
-        if (data.id != this.id) {
-            throw `Player ${this.id} received update meant for ${data.id}`
-        }
-        this.setRotation(data.angle)
-        this.setPosition(data.x, data.y)
-        this.vx = data.vx
-        this.vy = data.vy
-        this.omega = data.omega
-        this.alive = (data.flags & ALIVE_FLAG) != 0
-        this.braking = (data.flags & BRAKING_FLAG) != 0
-        this.boosting = (data.flags & BOOSTING_FLAG) != 0
-
-        //console.log(this.text)
-        this.text.setText(`${this.player_name} (${this.kills})`)
     }
 
     preUpdate(time: number, delta: number) {
@@ -124,51 +92,3 @@ export class Player extends GameObjects.Container {
         this.text.destroy()
     }
 }
-
-export interface UpdatePlayer {
-    id: integer
-    x: number
-    y: number
-    angle: number
-    vx: number
-    vy: number
-    omega: number
-    flags: integer
-}
-
-export function readUpdatePlayerFromStream(stream: ByteArrayInputStream): UpdatePlayer {
-    const id = stream.readShort()
-    const x = stream.readFloat() * 10
-    const y = stream.readFloat() * 10
-    const a = stream.readFloat()
-    const vx = stream.readFloat() * 10
-    const vy = stream.readFloat() * 10
-    const omega = stream.readFloat()
-    const flags = stream.readByte()
-
-    return {
-        id: id, 
-        x: x, 
-        y: y, 
-        angle: a, 
-        vx: vx,
-        vy: vy, 
-        omega: omega,
-        flags: flags
-    }
-}
-
-export interface InitialPlayer extends UpdatePlayer {
-    kills: integer
-    car_class: integer
-    name: string
-}
-
-export function readInitialPlayerFromStream(stream: ByteArrayInputStream): InitialPlayer {
-    const data = <InitialPlayer> readUpdatePlayerFromStream(stream)
-    data.car_class = stream.readByte()
-    data.kills = stream.readShort()
-    data.name = stream.readStringUntilNull()
-    return data
-}
-
