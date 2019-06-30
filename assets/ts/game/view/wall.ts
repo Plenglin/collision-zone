15d0188e00577fb08b79f36ebeaf8561ee09d92d
Max <maximumpwnguin@gmail.com>
2019-06-30 06:18:46 -0700

    Scene-related things

--- assets/ts/game/view/wall.ts
+++ assets/ts/game/view/wall.ts
@@ -1,21 +1,13 @@
 import { ByteArrayInputStream } from "../../util";
 import { GameObjects, Scene } from "phaser";
+import { Wall } from "../gamestate";
 
 
-export class Wall extends GameObjects.Rectangle {
-    constructor(scene: Scene, x: number, y: number, w: number, h: number, a: number) {
-        super(scene, x, y, w, h, 0xffffff)
-        this.rotation = a
+export class WallRenderer extends GameObjects.Rectangle {
+    constructor(scene: Scene, data: Wall) {
+        super(scene, data.x, data.y, data.w, data.h, 0xffffff)
+        this.rotation = data.a
         this.isFilled = true
         //this.setScale(w, h)
-    }
-
-    static readFromStream(scene: Scene, stream: ByteArrayInputStream): Wall {
-        const x = stream.readFloat() * 10
-        const y = stream.readFloat() * 10
-        const w = stream.readFloat() * 10
-        const h = stream.readFloat() * 10
-        const a = stream.readFloat()
-        return new Wall(scene, x, y, w, h, a)
     }
 }
