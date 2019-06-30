54b1aa20ceebb769f57fcbecffc563804ef5a03e
Max <maximumpwnguin@gmail.com>
2019-06-30 07:45:39 -0700

    Walls rendering properly

--- assets/ts/game/view/wall.ts
+++ assets/ts/game/view/wall.ts
@@ -5,7 +5,7 @@ import { Wall } from "../gamestate";
 
 export class WallRenderer extends GameObjects.Rectangle {
     constructor(scene: Scene, data: Wall) {
-        super(scene, data.x, data.y, data.w, data.h, 0xffffff)
+        super(scene, data.x * 10, data.y * 10, data.w * 10, data.h * 10, 0xffffff)
         this.rotation = data.a
         this.isFilled = true
         //this.setScale(w, h)
